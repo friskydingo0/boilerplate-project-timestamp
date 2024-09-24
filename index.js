eeeb73b65c5f382467b8cf2e39e5c1cfd9bbd1b0
friskydingo0 <19633116+friskydingo0@users.noreply.github.com>
2024-09-23 23:55:42 -0500

    Timestamp microservice sample

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,10 +21,27 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
-
+app.get('/api/:date?', (req, res) => {
+  let timestamp = null;
+  const dateParam = req.params.date;
+  if (dateParam !== undefined) {
+    timestamp = new Date(dateParam);
+    if (isNaN(timestamp)) {
+      timestamp = new Date(parseInt(dateParam));
+      if (isNaN(timestamp)) {
+        res.json({ "error": "Invalid Date" });
+        return;
+      }
+    }
+  }
+  else {
+    timestamp = new Date();
+  }
+  res.json({ unix: timestamp.getTime(), utc: timestamp.toUTCString() });
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
